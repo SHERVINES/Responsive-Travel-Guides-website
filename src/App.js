32d5f8233e0ce628c935f33e34a8b16f6ce698ed
shervin <shervin.ses@gmail.com>
2022-11-24 16:44:12 +0330

    cards

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import {Route,Routes} from "react-router-dom"
 import "./App.css"
+import Home from './components/pages/Home';
 
 const App = () => {
   return (
@@ -9,7 +10,7 @@ const App = () => {
     <Navbar/>
     <Routes>
 
-    <Route path="/" element={""} />
+    <Route path="/" element={<Home/>} />
       
       </Routes>
     </>
