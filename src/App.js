3babb6966a32fba42fefccd82fb0ff765cefa0a9
shervin <shervin.ses@gmail.com>
2022-11-28 19:52:17 +0330

    footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Navbar from './components/Navbar';
 import {Route,Routes} from "react-router-dom"
 import "./App.css"
 import Home from './components/pages/Home';
+import Services from "./components/pages/Services"
+import Products from "./components/pages/Products"
+import SignUp from "./components/pages/SignUp"
 
 const App = () => {
   return (
@@ -11,6 +14,9 @@ const App = () => {
     <Routes>
 
     <Route path="/" element={<Home/>} />
+    <Route path="/services" element={<Services/>} />
+    <Route path="/products" element={<Products/>} />
+    <Route path="/sign-up" element={<SignUp/>} />
       
       </Routes>
     </>
